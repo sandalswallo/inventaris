11ed214d024b391811c8a1cf864d909c9f46e2c2
sandalswallow <ridhoarekpunggul683@gmail.com>
2022-10-25 11:30:34 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    AuthController,
+    DashboardController,
+    BarangController,
+    TempatController
+};
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+// Route Login
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/postlogin', [AuthController::class, 'postlogin'])->name('postlogin');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Route Dashboard
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+// Route Barang
+Route::resource('/barang', BarangController::class);
+// Route Tempat
+Route::resource('/tempat', TempatController::class);
